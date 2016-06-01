c119a67a936a1ddf02defd0c1407138aafe2e61a
Sasha Kandrach <skkandrach@dyn-160-39-252-159.dyn.columbia.edu>
2016-06-01 11:45:31 -0400

    adding new README files to new homework assignments

---
+++ 01/homework-1-Sasha Kandrach.py
@@ -0,0 +1,86 @@
+#Sasha Kandrach
+#05/23/16
+#Homework 1
+year_of_birth = input("What year were you born?")
+    print("What year were you born?")
+
+age = 2016 - int(year_of_birth)
+print("You are roughly" + int(age)  + "years old")
+
+heartbeat_per_year = int(age) * 42,000,000
+heartbeat_of_Blue_Whale = int(age) *47 * 1440 *365
+heartbeat_of_Rabbit = int(age) *120 *1440 *365
+
+print("The average heart rate of a rabbit per minute has beaten"
+#http://rabbit.org/temperature-and-respiration-rates/
+print("The average heart rate of a Blue Whale at the same times has beaten")
+#http://www.whalefacts.org/blue-whale-heart/
+
+age_on_venus = int(age) 365/225
+age_on_neptune int(age) 365/60,190
+
+print("On Venus you are roughly", age_on_venus, "years old")
+print("On Neptune you are roughly", age_on_venus, "years old")
+
+if age > 19:
+    print("You are older than me.")
+if age == 19
+    print("You are the same age as me!")
+if age < 19:
+    print("You are younger than me.")
+else:
+    print("How old are you then?")
+older_than_me = int(age) -19
+younger_than_me = int(age) +19
+
+if year_of_birth num = int(input("Enter a number: "))
+    if (num % 2) == 0:
+   print("You were born in an even year")
+else:
+   print("You were born in an odd year.")
+
+   if year_of_birth = 1974, 1975, 1978, 1979, 2005, 2008
+   print("You were unfortunately born in the year the Steelers won the Super Bowl")
+else:
+    print("Lucky you! You weren't born in the same year that the Steelers won the Super Bowl")
+
+if int(year_of_birth) >= 2009:
+  print("You were born during Obama's Presidency - fellow Columbia alum.")
+
+elif int(year_of_birth) >= 2001:
+  print("You were born during George W. Bush's Presidency.")
+
+elif int(year_of_birth) >= 1993:
+  print("You were born during Clinton's Presidency")
+
+elif int(year_of_birth) >= 1989:
+  print("You were born during Bush Senior's Presidency")
+
+elif int(year_of_birth) >= 1981:
+  print("You were born during Reagan's Presidency")
+
+elif int(year_of_birth) >= 1977:
+  print("You were born during Carter's Presidency")
+
+elif int(year_of_birth) >= 1974:
+  print("You were born during Ford's Presidency")
+
+elif int(year_of_birth) >= 1969:
+  print("You were born during Nixon's Presidency")
+
+elif int(year_of_birth) >= 1963:
+  print("You were born during LBJ's Presidency")
+
+elif int(year_of_birth) >= 1961:
+  print("You were born during JFK's Presidency")
+
+elif int(year_of_birth) >= 1953:
+  print("You were born during Eisenhower's Presidency")
+
+elif int(year_of_birth) >= 1945:
+  print("You were born during Truman's Presidency")
+
+elif int(year_of_birth) >= 1933:
+   print("You were born during FDR's Presidency.")
+else:
+    year_of_birth = input("What year were you born?")
